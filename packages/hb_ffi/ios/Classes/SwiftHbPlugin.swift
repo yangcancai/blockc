5966b72aa267483c3b91b003637df39a93a69de4
admin <admin.com>
2021-11-08 21:33:54 +0800

    fix: Add dummyMethodToEnforceBundling to  make bundling in ios easier

--- packages/hb_ffi/ios/Classes/SwiftHbPlugin.swift
+++ packages/hb_ffi/ios/Classes/SwiftHbPlugin.swift
@@ -11,4 +11,7 @@ public class SwiftHbPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+  public static func dummyMethodToEnforceBundling() {
+    last_error_length()
+  }
 }
